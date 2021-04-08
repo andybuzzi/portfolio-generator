dad822b80e6516434ffda9be3c663215f3dcc5fa
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-04-08 17:23:59 -0500

    html file generated

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// to use fs module this needs to be written at the top of the file
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+// FIRST ARG: file name, SECOND ARG: the data that's being written. THIRD ARG: callback function to handle any errors.
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the input!");
+});
